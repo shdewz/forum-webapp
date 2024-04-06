a8218c2bf6f868f1ae5849fe651c660ec10009db
shdewz <shdewz@gmail.com>
2024-04-06 20:20:08 +0300

    Add js dependencies

---
+++ static/dist/timeago-locales.js
@@ -0,0 +1,33 @@
+const timeago_locales = {
+    fi: {
+        suffixAgo: 'sitten',
+        suffixFromNow: 'tulevaisuudessa',
+        seconds: 'alle minuutti',
+        minute: 'minuutti',
+        minutes: '%d minuuttia',
+        hour: 'tunti',
+        hours: '%d tuntia',
+        day: 'päivä',
+        days: '%d päivää',
+        month: 'kuukausi',
+        months: '%d kuukautta',
+        year: 'vuosi',
+        years: '%d vuotta'
+    },
+    en: {
+        suffixAgo: 'ago',
+        suffixFromNow: 'from now',
+        inPast: 'any moment now',
+        seconds: 'less than a minute',
+        minute: 'a minute',
+        minutes: '%d minutes',
+        hour: 'an hour',
+        hours: '%d hours',
+        day: 'a day',
+        days: '%d days',
+        month: 'a month',
+        months: '%d months',
+        year: 'a year',
+        years: '%d years',
+    }
+};
